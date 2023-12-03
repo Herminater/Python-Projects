ea740ddb179c28ef9378388dd16b34e20b4fa412
Herminater <99097317+Herminater@users.noreply.github.com>
2023-12-03 13:48:19 +0100

    Update settings.py

--- scheduler_app/scheduler_app/settings.py
+++ scheduler_app/scheduler_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6gy@&e$*azw4!x6k)8+ul1&p@-ic!8i#d$a1e=xa5-murgsa2'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
